93bf0cf2955a19f1383b4b284aa840e24bb3c558
Royal <royal.21ai040@sode-edu.in>
2023-09-30 17:31:11 +0530

    test

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: "AIzaSyBCDLgBbc0vflon7p7Rd9-o1-LgqrSsW38",
+  authDomain: "linkedin-clone-358df.firebaseapp.com",
+  projectId: "linkedin-clone-358df",
+  storageBucket: "linkedin-clone-358df.appspot.com",
+  messagingSenderId: "292966018836",
+  appId: "1:292966018836:web:999b5f3d0678ddb8b1dc01",
+  measurementId: "G-VH2RLVDB3E",
 };
-console.log(firebaseConfig);
-
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
 const auth = getAuth(firebaseApp);
